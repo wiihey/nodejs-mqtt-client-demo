9966a0e90e7618e024d08dec639c7dbfa337d141
Junfeng Geng <junfeng@wiihey.com>
2018-06-26 14:29:53 +0800

    Initial

---
+++ src/index.js
@@ -0,0 +1,31 @@
+var mqtt = require("mqtt");
+
+// Please change APP_ID and APP_KEY to those provided by WiiHey Customer Service
+var APP_ID = "demo";
+var APP_KEY = "demo123";
+
+var PORT = 8883;
+var HOST = "iot.wiihey.com";
+
+var options = {
+  port: PORT,
+  host: HOST,
+  username: APP_ID,
+  password: APP_KEY,
+  protocol: "mqtts",
+  rejectUnauthorized: true
+};
+
+var client = mqtt.connect(options);
+
+client.subscribe("apps/" + APP_ID + '/inbox');
+
+client.on("connect", function() {
+  console.log("Connection Established");
+});
+
+client.on("message", function(topic, message) {
+  console.log("Received: " + message.toString());
+
+  // Process Received Message
+});
